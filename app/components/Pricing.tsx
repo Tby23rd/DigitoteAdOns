122f0a3fe8ce9095aca38f1d241f3eab6b830dd6
tby23rd <tabitha.maru@gmail.com>
2023-11-24 21:36:21 -0700

    Random

--- app/components/Pricing.tsx
+++ app/components/Pricing.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 import Card from './Card';
 import styles from './Pricing.module.css';
 
-
-
 const Pricing: React.FC = () => {
   return (
     <div className={styles.grid1}>
